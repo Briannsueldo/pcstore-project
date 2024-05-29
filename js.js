3120357f1a34158fd3b8199de0cbc9d9df3e164f
Brian Sueldo <briannsueldo@gmail.com>
2024-05-28 23:10:59 -0300

    Section update and footer fix

--- js.js
+++ js.js
@@ -5,3 +5,32 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mainMenu.classList.toggle("active");
 })
+
+//
+
+const problemContainer = document.querySelector(".problem-container");
+const problemSpan = document.querySelector(".problem-container span");
+const problemContainerMenu = document.querySelector(".problem-options-container");
+const selectOption = document.querySelector(".select-option");
+const problemOption = document.querySelectorAll(".problem-option");
+
+selectOption.addEventListener("click", () => {
+    selectOption.classList.toggle("active");
+    problemContainer.classList.toggle("active");
+    problemContainerMenu.classList.toggle("active");
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    problemOption.forEach(option => {
+        option.addEventListener("click", function () {
+            
+            problemContainer.textContent = this.textContent;
+
+            problemContainer.style.fontWeight = '500';
+            problemContainer.style.color = '#212121';
+
+            selectOption.classList.remove("open");
+        })
+    })
+})
